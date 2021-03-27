04420375e27dcbeee33f472beafe3e39dd8b19c5
sarode27 <81471235+sarode27@users.noreply.github.com>
2021-03-27 17:59:31 +0530

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,19 @@
+
+#     Application 3:              " Building Personal Website with Python and flask."
+
+
+
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
